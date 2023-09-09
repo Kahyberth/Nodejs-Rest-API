4ccf0ad382d6369df090f6ccb8ab3ea3bf7b3406
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-09 16:17:07 -0500

    fix env

--- src/config.js
+++ src/config.js
@@ -7,6 +7,6 @@ export default {
     DB_HOST: process.env.DB_HOST || "localhost",
     DB_USER: process.env.DB_USER || "root",
     DB_PASSWORD: process.env.DB_PASSWORD || "admin",
-    DB_NAME: process.env.DATABASE || "companydb",
+    DB_NAME: process.env.DB_DATABASE || "companydb",
     DB_PORT: process.env.DB_PORT || 3306,
 }
